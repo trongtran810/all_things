1ccf64697015d295ab15a8277f6ec440a0e0b2b9
Trong Tran <gacontuyenchien1@gmail.com>
2023-04-17 23:01:29 +0700

    add decode from []byte to struct

--- utils/encoding.go
+++ utils/encoding.go
@@ -67,37 +67,6 @@ func Bytes(v interface{}) []byte {
 	return buf.Bytes()
 }
 
-// The MustUpdate function recursively checks if each field of
-// a struct is zero(is not set in config file), and if so, sets it to the corresponding value in another struct.
-func MustUpdate(v reflect.Value, vDef reflect.Value) bool {
-	if v.Kind() != reflect.Struct {
-		if v.IsZero() {
-			return true
-		}
-	} else {
-		for i := 0; i < v.NumField(); i++ {
-			u := MustUpdate(v.Field(i), vDef.Field(i))
-			if u {
-				t := vDef.Field(i)
-				v.Field(i).Set(t)
-			}
-		}
-	}
-	return false
-}
-
-// The "UpdateHeader" method uses "MustUpdate" to
-// update the fields of a struct from another struct
-// that have the same type
-func UpdateHeader(h interface{}, tmp interface{}) {
-	v := reflect.ValueOf(h).Elem()
-	vNew := reflect.ValueOf(tmp).Elem()
-	if v.Type() != vNew.Type() {
-		panic("UpdateHeader: incompatible types")
-	}
-	MustUpdate(v, vNew)
-}
-
 func TestEncoding() {
 	x := MyType1{V1: 1, V2: MyType2{V2: 2}, Ip: [4]uint8{192, 168, 1, 0}}
 	xBef := MyType1{V1: 1, V2: MyType2{V2: 2}, Ip: [4]uint8{192, 168, 1, 0}}
