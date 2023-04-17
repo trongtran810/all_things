41e1e4c48d3597dc0370573df2fff3ac015debcb
Trong Tran <gacontuyenchien1@gmail.com>
2023-04-17 08:21:38 +0700

    codec, func file releated. Rebase

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	// utils.WatchFileChange("D:\\TrongTran\\Job\\Reactjs\\golang\\test\\data\\watch_file_change_test.txt")
 	// utils.TestDecode()
-	utils.TestEncoding()
+	// utils.TestEncoding()
 	fmt.Println()
+	utils.WatchFileChange("D:\\TrongTran\\Job\\Reactjs\\golang\\test\\data\\watch_file_change_test.txt")
 }
