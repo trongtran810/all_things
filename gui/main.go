cc43f037af1d2e75b909df778a794b8f44d071e4
QuanNH <quannh@meta-vi.vn>
2023-05-25 16:22:54 +0700

    Add data type tooltip

--- gui/main.go
+++ gui/main.go
@@ -6,12 +6,13 @@ import (
 	"reflect"
 	"strings"
 
-	_ "github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/gui"
 	"github.com/therecipe/qt/widgets"
 )
 
 type AddrOther struct {
-	one string
+	One string
 }
 type Address struct {
 	Street  string
@@ -138,7 +139,30 @@ func createInputWidget(fieldValue reflect.Value) widgets.QWidget_ITF {
 	// 	return input
 	default:
 		input := widgets.NewQLineEdit(nil)
-		input.SetText(fieldValue.String())
+		input.SetText(fmt.Sprintf("%v", fieldValue.Interface()))
+
+		tooltip := widgets.NewQWidget(nil, 0)
+		tooltip.SetWindowFlags(core.Qt__ToolTip | core.Qt__FramelessWindowHint)
+		tooltip.SetAttribute(core.Qt__WA_TranslucentBackground, true)
+		tooltip.SetAttribute(core.Qt__WA_ShowWithoutActivating, true)
+		tooltip.SetAttribute(core.Qt__WA_DeleteOnClose, true)
+		tooltipLayout := widgets.NewQVBoxLayout()
+		tooltipLayout.SetContentsMargins(5, 5, 5, 5)
+		tooltipContent := widgets.NewQLabel(nil, 0)
+		tooltipContent.SetStyleSheet("background-color: yellow;")
+		tooltipLayout.AddWidget(tooltipContent, 0, 0)
+		tooltip.SetLayout(tooltipLayout)
+
+		input.ConnectFocusInEvent(func(event *gui.QFocusEvent) {
+			tooltipContent.SetText(fieldValue.Type().String())
+			tooltip.Move(input.MapToGlobal(core.NewQPoint2(0, input.Height())))
+			tooltip.Show()
+		})
+
+		input.ConnectFocusOutEvent(func(event *gui.QFocusEvent) {
+			tooltip.Hide()
+		})
+
 		return input
 	}
 
@@ -171,285 +195,3 @@ func outputData(layout *widgets.QFormLayout, data reflect.Value, wgMapping WgMap
 		}
 	}
 }
-
-// func outputData(layout *widgets.QFormLayout, data any, prefix string) {
-// 	value := reflect.ValueOf(data).Elem()
-
-// 	for i := 0; i < layout.Count(); i++ {
-// 		item := layout.ItemAt(i)
-// 		widget := item.Widget()
-
-// 		if groupBox, ok := (*widgets.QGroupBox)(widget); ok {
-// 			groupBoxLayout := groupBox.Layout()
-// 			outputData(groupBoxLayout.(*widgets.QFormLayout), value.Field(i).Addr().Interface(), "")
-// 		} else {
-// 			label := item.Widget().(*widgets.QLabel)
-// 			fieldName := label.Text()
-
-// 			if fieldValue := value.Field(i); fieldValue.IsValid() {
-// 				fmt.Printf("%s: %v\n", fieldName, fieldValue.Interface())
-// 			}
-// 		}
-// 	}
-// }
-
-// package main
-
-// type Person struct {
-// 	Name   string
-// 	Age    int
-// 	Active bool
-// }
-
-// func main() {
-// 	app := widgets.NewQApplication(len(os.Args), os.Args)
-
-// 	// persons := []Person{
-// 	// 	{Name: "John", Age: 30, Active: true},
-// 	// 	{Name: "Jane", Age: 25, Active: false},
-// 	// 	{Name: "Mike", Age: 35, Active: true},
-// 	// }
-
-// 	dialog := widgets.NewQDialog(nil, 0)
-// 	layout := widgets.NewQFormLayout(dialog)
-
-// 	// Get the type of the struct
-// 	structType := reflect.TypeOf(Person{})
-
-// 	// Iterate over the struct fields
-// 	for i := 0; i < structType.NumField(); i++ {
-// 		field := structType.Field(i)
-// 		fieldName := field.Name
-// 		// Create the corresponding row label
-// 		rowLabel := widgets.NewQLabel(nil, 0)
-// 		rowLabel.SetText(fieldName + ":")
-// 		// Create an input widget based on the field type
-// 		var inputWidget widgets.QWidget_ITF
-// 		switch field.Type.Kind() {
-// 		case reflect.String:
-// 			lineEdit := widgets.NewQLineEdit(nil)
-// 			inputWidget = lineEdit
-// 			layout.AddRow(rowLabel, lineEdit)
-// 		case reflect.Int:
-// 			spinBox := widgets.NewQSpinBox(nil)
-// 			inputWidget = spinBox
-// 			layout.AddRow(rowLabel, spinBox)
-// 		case reflect.Bool:
-// 			checkBox := widgets.NewQCheckBox(nil)
-// 			inputWidget = checkBox
-// 			layout.AddRow(rowLabel, checkBox)
-// 		default:
-// 			fmt.Println("Unsupported field type:", field.Type)
-// 		}
-
-// 		layout.AddRow(rowLabel, inputWidget)
-
-// 		// Store the row label and input widget in the map
-// 		rowLabels[rowLabel] = inputWidget
-// 	}
-
-// 	dialog.SetLayout(layout)
-// 	dialog.Show()
-
-// 	app.Exec()
-// }
-// package main
-
-// import (
-// 	"os"
-
-// 	"github.com/therecipe/qt/gui"
-// 	"github.com/therecipe/qt/widgets"
-// )
-
-// func main() {
-// 	app := widgets.NewQApplication(len(os.Args), os.Args)
-
-// 	// Create a QTreeView
-// 	treeView := widgets.NewQTreeView(nil)
-
-// 	// Create a QStandardItemModel
-// 	model := gui.NewQStandardItemModel(nil)
-// 	model.SetHorizontalHeaderLabels([]string{"Name", "Value"})
-
-// 	// Populate the model with data
-// 	rootNode := model.InvisibleRootItem()
-
-// 	// First node
-// 	node1 := gui.NewQStandardItem2("Node 1")
-// 	node1.SetColumnCount(2)
-// 	node1.SetChild(0, 0, gui.NewQStandardItem2("Subnode 1"))
-// 	node1.SetChild(0, 1, gui.NewQStandardItem2("Value 1"))
-
-// 	// Second node
-// 	node2 := gui.NewQStandardItem2("Node 2")
-// 	node2.SetColumnCount(2)
-// 	node2.SetChild(0, 0, gui.NewQStandardItem2("Subnode 2"))
-// 	node2.SetChild(0, 1, gui.NewQStandardItem2("Value 2"))
-
-// 	rootNode.AppendRow2(node1)
-// 	rootNode.AppendRow2(node2)
-
-// 	// Set the model
-// 	treeView.SetModel(model)
-// 	treeView.ExpandAll()
-
-// 	// Show the treeView
-// 	treeView.Show()
-
-// 	app.Exec()
-// }
-
-// package main
-
-// import (
-// 	"os"
-
-// 	"github.com/therecipe/qt/widgets"
-// )
-
-// type Person struct {
-// 	Name    string
-// 	Friends []Person
-// }
-
-// func main() {
-// 	widgets.NewQApplication(len(os.Args), os.Args)
-
-// 	window := widgets.NewQMainWindow(nil, 0)
-// 	window.SetWindowTitle("Nested Struct Example")
-
-// 	treeWidget := widgets.NewQTreeWidget(nil)
-// 	treeWidget.SetColumnCount(1)
-
-// 	persons := []Person{
-// 		{
-// 			Name: "Alice",
-// 			Friends: []Person{
-// 				{Name: "Bob"},
-// 				{Name: "Charlie"},
-// 			},
-// 		},
-// 		{
-// 			Name: "Dave",
-// 			Friends: []Person{
-// 				{Name: "Ed"},
-// 				{Name: "Frank"},
-// 			},
-// 		},
-// 	}
-
-// 	for _, person := range persons {
-// 		addItem(treeWidget.InvisibleRootItem(), person)
-// 	}
-
-// 	window.SetCentralWidget(treeWidget)
-// 	window.Show()
-
-// 	widgets.QApplication_Exec()
-// }
-
-// func addItem(parent *widgets.QTreeWidgetItem, person Person) {
-// 	item := widgets.NewQTreeWidgetItem6(parent, 0)
-// 	item.SetText(0, person.Name)
-
-// 	for _, friend := range person.Friends {
-// 		child := widgets.NewQTreeWidgetItem2(friend.Name, 0)
-// 		// label := widgets.NewQLabel2(friend.Name, nil, 0)
-// 		lineEdit := widgets.NewQLineEdit(nil)
-// 		// item.TreeWidget().SetItemWidget(child, 0, label)
-// 		item.TreeWidget().SetItemWidget(child, 1, lineEdit)
-// 		// item.AddChild(child)
-// 		// child.SetText(0, friend.Name)
-// 	}
-// }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"reflect"
-// )
-
-// type Address struct {
-// 	Street  string
-// 	City    string
-// 	Country string
-// }
-
-// type Person struct {
-// 	Name    string
-// 	Age     int
-// 	Active  bool
-// 	Four    string
-// 	Address Address
-// }
-
-// func PrintStructValue(v reflect.Value) {
-// 	for i := 0; i < v.NumField(); i++ {
-// 		f := v.Field(i)
-
-// 		switch f.Kind() {
-// 		case reflect.Struct:
-// 			PrintStructValue(f)
-// 		default:
-// 			fmt.Println(f)
-// 		}
-// 	}
-// }
-
-// func main() {
-// 	s := Person{
-// 		Name:   "John",
-// 		Age:    28,
-// 		Active: true,
-// 		Four:   "Four",
-// 		Address: Address{
-// 			Street:  "123 Main St",
-// 			City:    "Anywhere",
-// 			Country: "USA",
-// 		},
-// 	}
-
-// 	v := reflect.ValueOf(&s).Elem()
-// 	PrintStructValue(v)
-// }
-
-// package main
-
-// import (
-// 	"os"
-
-// 	"github.com/therecipe/qt/widgets"
-// )
-
-// func main() {
-// 	app := widgets.NewQApplication(len(os.Args), os.Args)
-
-// 	// create a new widget
-// 	window := widgets.NewQWidget(nil, 0)
-
-// 	// create a vertical layout
-// 	layout := widgets.NewQVBoxLayout2(window)
-
-// 	// create a QLabel and add it to the layout
-// 	label := widgets.NewQLabel2("Above the line", nil, 0)
-// 	layout.AddWidget(label, 0, 0)
-
-// 	// create a QFrame, set it as a line, and add it to the layout
-// 	line := widgets.NewQFrame(nil, 0)
-// 	line.SetFrameShape(widgets.QFrame__HLine) // Horizontal line
-// 	line.SetFrameShadow(widgets.QFrame__Sunken)
-// 	layout.AddWidget(line, 0, 0)
-
-// 	// create another QLabel and add it to the layout
-// 	label2 := widgets.NewQLabel2("Below the line", nil, 0)
-// 	layout.AddWidget(label2, 0, 0)
-
-// 	// set layout to the window
-// 	window.SetLayout(layout)
-// 	window.Show()
-
-// 	// start the application
-// 	app.Exec()
-// }
